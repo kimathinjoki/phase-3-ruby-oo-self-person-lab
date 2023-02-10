c31b01dfc18c2b0a4ba85725c886a4d74be9111d
kimathinjoki <dfkimathinjoki@gmail.com>
2023-02-10 09:50:57 -0800

    almost done

--- lib/person.rb
+++ lib/person.rb
@@ -5,8 +5,8 @@ class Person
     attr_reader :name 
     attr_accessor :bank_account
     attr_accessor :happiness
-    attr_reader :hygiene
-    # attr_accessor :bank_account :hapiness :hygiene
+    # attr_writer :happiness
+    attr_accessor :hygiene
 
 
  
@@ -22,15 +22,98 @@ class Person
     #     @bank_account = bank_account
     # end
 
-    # def hapiness(happiness)
-    #     @happiness = happinnes
+    # def happiness=(happiness)
+        
+        # if @happinnes > 10
+        #     @happiness = 10
+        # elsif @happiness < 0
+        #     @happiness = 0
+        # else 
+        #     @happinnes = happiness
+        # end 
+    #     @happiness = happiness
+        
+     
+
+
     # end
 
-    def hygiene=(hygiene)
-        if self.hygiene < 11 && self.hygiene > -1
-            self.hygiene =hygiene
+    # def happiness
+
+        # if @happinnes > 10
+        #     @happiness = 10
+        # elsif @happiness < 0
+        #     @happiness = 0
+        # else 
+        #     @happinnes
+        # end 
+    #     @happiness
+    
+
+    # end
+
+    # def hygiene(hygiene)
+    #     if @hygiene <=10 && @hygiene >= 0
+    #         @hygiene =hygiene
+    #     end
+    # end
+
+    def clean?
+        if self.hygiene > 7 && self.hygiene < 10
+            true
+        else 
+            false
         end
     end
 
+    def happy? 
+        if @happiness > 7 
+            true
+        else
+            false
+        end
+        # unless self.happiness > 7
+        #     false
+        # else 
+        #     true
+        # end
+        
+    end
+
+    def get_paid(amount)
+        self.bank_account += amount 
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.happiness += 2
+        self.hygiene -= 3
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend = self.new(str))
+        friend.happiness += 3
+        @happiness += 3
+        "Hi #{friend.name}! It's #{@name}. How are you?"
+    end
+
+    def start_conversation(friend = self.new(str), topic)
+        if topic == "politics"
+        friend.happiness -= 2
+        self.happiness -=2
+        "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            friend.happiness += 1
+            self.happiness +=1
+            "blah blah sun blah rain"
+        elsif topic != "politics" && topic != "weather"
+            "blah blah blah blah blah"
+        end
+    end
 
 end
